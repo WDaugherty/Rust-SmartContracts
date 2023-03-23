36adab419111450e0a437951ba3a3fccc2ed88d9
Will Daugherty <wld37@cornell.edu>
2023-03-22 20:06:47 -0400

    Initial Setup

---
+++ SMC1/target/debug/deps/Prob4-24e1251796aefe37.d
@@ -0,0 +1,5 @@
+/Users/wdaugherty/Cornell_Tech_DL/CS-5787/HW2/rust_scripts/Prob4/target/debug/deps/Prob4-24e1251796aefe37: src/main.rs
+
+/Users/wdaugherty/Cornell_Tech_DL/CS-5787/HW2/rust_scripts/Prob4/target/debug/deps/Prob4-24e1251796aefe37.d: src/main.rs
+
+src/main.rs:
